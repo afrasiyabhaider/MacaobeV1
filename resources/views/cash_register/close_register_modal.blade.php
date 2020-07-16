b2861d7571979aef2e1d178d18210e917f61f7fd
Afrasiyab Haider <afrasiyab555@gmail.com>
2020-07-16 23:54:56 +0500

    Update close_register_modal.blade.php

--- resources/views/cash_register/close_register_modal.blade.php
+++ resources/views/cash_register/close_register_modal.blade.php
@@ -24,7 +24,8 @@
                 @lang('cash_register.cash_payment'):
               </th>
               <td>
-                <span class="display_currency" data-currency_symbol="true">{{ $register_details->total_cash }}</span>
+                <span class="display_currency" data-currency_symbol="true">{{ $transactions->sum('final_total')+$details['transaction_details']->total_discount}}</span>
+                {{-- <span class="display_currency" data-currency_symbol="true">{{ $register_details->total_cash }}</span> --}}
               </td>
             </tr>
             {{-- <tr>
@@ -49,7 +50,7 @@
                 Discount Given:
               </td>
               <td>
-                <span class="display_currency" data-currency_symbol="true">{{ $discount}}</span>
+                <span class="display_currency" data-currency_symbol="true">{{ $discount }}</span>
               </td>
             </tr>
             <tr>
@@ -124,11 +125,13 @@
             </tr> --}}
             <tr>
               <td>
-                @lang('cash_register.total_sales'):
+                @lang('cash_register.total_cash')
               </td>
               <td>
-                <span class="display_currency" data-currency_symbol="true">{{ ($card + $register_details->total_cash ) }}</span>
+                <span class="display_currency" data-currency_symbol="true">{{$transactions->sum('final_total') + $cash_in_hand}}</span>
+                {{-- <b><span class="display_currency" data-currency_symbol="true">{{ $register_details->cash_in_hand + $register_details->total_cash - $register_details->total_cash_refund }}</span></b> --}}
               </td>
+              
             </tr>
             <tr class="success">
               <th>
@@ -166,10 +169,11 @@
             </tr>
             <tr class="success">
               <th>
-                @lang('cash_register.total_cash')
+                @lang('cash_register.total_sales'):
               </th>
               <td>
-                <b><span class="display_currency" data-currency_symbol="true">{{ $register_details->cash_in_hand + $register_details->total_cash - $register_details->total_cash_refund }}</span></b>
+                {{-- <span class="display_currency" data-currency_symbol="true">{{ ($card + $register_details->total_cash) }}</span> --}}
+                <b class="display_currency" data-currency_symbol="true">{{ $register_details->total_sale }}</b>
               </td>
             </tr>
           </table>
