b92d5eddd6f3356ec5168ee44b77a1571a176454
Afrasiyab Haider <afrasiyab555@gmail.com>
2020-07-23 15:34:45 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -279,10 +279,10 @@ Route::middleware(['IsInstalled', 'auth', 'SetSessionData', 'language', 'timezon
 
     Route::get('/cash-register/register-details', 'CashRegisterController@getRegisterDetails');
 
-    Route::get('/cash-register/close-register', 'CashRegisterController@getCloseRegister');
-    Route::post('/cash-register/close-register', 'CashRegisterController@postCloseRegister');
+    // Route::get('/cash-register/close-register', 'CashRegisterController@getCloseRegister');
+    // Route::post('/cash-register/close-register', 'CashRegisterController@postCloseRegister');
 
-    Route::get('/cash-register/auto-close', 'CashRegisterController@autoCloseRegister');
+    // Route::get('/cash-register/auto-close', 'CashRegisterController@autoCloseRegister');
 
 
     Route::resource('cash-register', 'CashRegisterController');
