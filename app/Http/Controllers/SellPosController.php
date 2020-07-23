6a990378b4cd28845c9ede643cbb990e29d35a07
Afrasiyab Haider <afrasiyab555@gmail.com>
2020-07-23 15:18:25 +0500

    Update SellPosController.php

--- app/Http/Controllers/SellPosController.php
+++ app/Http/Controllers/SellPosController.php
@@ -395,11 +395,11 @@ class SellPosController extends Controller
             $is_direct_sale = true;
         }
 
-        dd($this->cashRegisterUtil->countOpenedRegister(),request()->session()->get('user.business_location_id'));
+        // dd($this->cashRegisterUtil->countOpenedRegister(),request()->session()->get('user.business_location_id'));
         //Check if there is a open register, if no then redirect to Create Register screen.
-        // if (!$is_direct_sale && $this->cashRegisterUtil->countOpenedRegister() == 0) {
-        //     return redirect()->action('CashRegisterController@create');
-        // }
+        if (!$is_direct_sale && $this->cashRegisterUtil->countOpenedRegister() == 0) {
+            return redirect()->action('CashRegisterController@create');
+        }
 
         try {
             $input = $request->except('_token');
