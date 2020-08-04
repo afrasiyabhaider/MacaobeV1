d58f8e19e1336eb2e6e07bb334851fa833f87a5d
Afrasiyab Haider <afrasiyab555@gmail.com>
2020-08-05 01:31:52 +0500

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2725,7 +2725,7 @@ class ProductController extends Controller
                             $new_qty = $before_transfer_qty + $productQty; 
 
                             $after_transfer->update(['qty_available' => $new_qty]);
-                            $after_transfer->update(['product_update_at'=>Carbon::now()]);
+                            $after_transfer->update(['product_updated_at'=>Carbon::now()]);
                         }
                         DB::commit();
                     } else {
